c3cc6ee5515167ab1da6720bcbd36eadf995fbdd
Christopher Spencer <cmspencer109@gmail.com>
2017-12-04 19:17:22 -0600

    Problem 7 & 8

---
+++ 2017/07.js
@@ -0,0 +1,36 @@
+// Day 4, Puzzle 7
+
+const fs = require('fs');
+
+let numberOfValidPassphrases = 0;
+
+fs.readFile('07.txt', function(err, data) {  
+	if (err) throw err;
+	
+	let array = data.toString().split("\n");
+	
+	for (i in array) {
+		let passphrase = array[i]
+		
+		if (passphraseIsValid(passphrase)) {
+			numberOfValidPassphrases++
+		}
+	}
+	
+	console.log(numberOfValidPassphrases)
+});
+
+function passphraseIsValid(input) {
+	const words = input.split(" ")
+	let isValid = true
+
+	for (let i = 0; i < words.length; i++) {
+		for (let j = i + 1; j < words.length; j++) {
+			if (words[i] == words[j]) {
+				isValid = false;
+			}
+		}
+	}
+
+	return isValid
+}
